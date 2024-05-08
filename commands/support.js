2e96b4f0ec96ba4f10cefc0f004fed301676ce46
rdean3107 <rdean3107@gmail.com>
2024-05-08 14:13:42 +0700

    init project

---
+++ commands/support.js
@@ -0,0 +1,73 @@
+/*
+
+  ________.__                        _____.___.___________
+ /  _____/|  | _____    ____  ____   \__  |   |\__    ___/
+/   \  ___|  | \__  \ _/ ___\/ __ \   /   |   |  |    |   
+\    \_\  \  |__/ __ \\  \__\  ___/   \____   |  |    |   
+ \______  /____(____  /\___  >___  >  / ______|  |____|   
+        \/          \/     \/    \/   \/                  
+
+╔════════════════════════════════════════════════════════════════════════╗
+║                                                                        ║
+║  ## Created by GlaceYT!                                                ║
+║  ## Feel free to utilize any portion of the code                       ║
+║  ## DISCORD :  https://discord.com/invite/xQF9f9yUEM                   ║
+║  ## YouTube : https://www.youtube.com/@GlaceYt                         ║
+║                                                                        ║
+╚════════════════════════════════════════════════════════════════════════╝
+
+
+*/
+
+const { EmbedBuilder } = require('discord.js');
+
+module.exports = {
+  name: "support",
+  description: "Get support server link",
+  permissions: "0x0000000000000800",
+  options: [],
+  run: async (client, interaction) => {
+    try {
+
+      const supportServerLink = "https://discord.gg/pausedayyy";
+      const instagramLink = "https://www.instagram.com/_tuiladuyanh/";
+      const facebookLink = "https://facebook.com/doanphamduyanhhh";
+      const discordLink = "https://discord.gg/pausedayyy";
+        const embed = new EmbedBuilder()
+            .setColor('#b300ff')
+            .setAuthor({
+              name: 'Support Server',
+              iconURL: 'https://cdn.discordapp.com/attachments/1230824451990622299/1230824519220985896/6280-2.gif?ex=6638ae28&is=66375ca8&hm=13e4a1b91a95b2934a39de1876e66c11711c7b30ac1a91c2a158f2f2ed1c2fc6&', 
+              url: 'https://discord.gg/xQF9f9yUEM'
+          })
+            .setDescription(`➡️ **Join our Discord server for support and updates:**\n- Discord - ${supportServerLink}\n\n➡️ **Follow us on:**\n- Instagram - ${instagramLink}\n- Facebook - ${facebookLink}\n- YouTube - ${discordLink}`)
+            .setImage('https://cdn.discordapp.com/attachments/1211355355079254138/1215148642659467336/a08282e9e8894228a74a00b7fdc2de01_00_00_00-00_00_02.gif?ex=6639a8dc&is=6638575c&hm=3545db8ef258d3e226480362c30801da470308154633fe0b12c8288eeba73c2f&')
+            .setTimestamp();
+
+      return interaction.reply({ embeds: [embed] });
+    } catch (e) {
+    console.error(e); 
+  }
+  },
+};
+
+/*
+
+  ________.__                        _____.___.___________
+ /  _____/|  | _____    ____  ____   \__  |   |\__    ___/
+/   \  ___|  | \__  \ _/ ___\/ __ \   /   |   |  |    |   
+\    \_\  \  |__/ __ \\  \__\  ___/   \____   |  |    |   
+ \______  /____(____  /\___  >___  >  / ______|  |____|   
+        \/          \/     \/    \/   \/                  
+
+╔════════════════════════════════════════════════════════════════════════╗
+║                                                                        ║
+║  ## Created by GlaceYT!                                                ║
+║  ## Feel free to utilize any portion of the code                       ║
+║  ## DISCORD :  https://discord.com/invite/xQF9f9yUEM                   ║
+║  ## YouTube : https://www.youtube.com/@GlaceYt                         ║
+║                                                                        ║
+╚════════════════════════════════════════════════════════════════════════╝
+
+
+*/
